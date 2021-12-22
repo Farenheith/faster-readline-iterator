d74db8760286397fb7714a696484e8b1407626e5
Thiago Santos <tos.oliveira@gmail.com>
2021-12-22 00:08:07 -0300

    feat: testing another strategy

--- index.d.ts
+++ index.d.ts
@@ -1,3 +1,5 @@
+import { Readable } from 'stream';
 import { Interface } from 'readline';
 
 export declare function getReadlineIterable<T = any>(readable: Interface): AsyncIterable<T>;
+export declare function getLineIterable<T = any>(readable: Readable): AsyncIterable<T>;
